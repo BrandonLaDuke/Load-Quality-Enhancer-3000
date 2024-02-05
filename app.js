95a4421332b83f942d055f3c85d9f251600f9816
Brandon LaDuke <brandon@brandonladuke.com>
2024-02-04 23:06:59 -0500

    Remove score overwrite

--- app.js
+++ app.js
@@ -266,7 +266,6 @@ document.addEventListener('DOMContentLoaded', () => {
     //game over
     function gameOver() {
       if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-        scoreDisplay.innerHTML = 'end'
         clearInterval(timerId)
       }
     }
